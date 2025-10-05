ac4b5f8c11af469363553a82971f189d55da41d3
David β <54827653+dmbeta@users.noreply.github.com>
2025-10-05 17:24:52 -0400

    add -y

--- install_docker.sh
+++ install_docker.sh
@@ -1,16 +1,16 @@
 # remove debian-provided packages
-apt remove docker-compose docker docker.io containerd runc
+apt remove docker-compose docker docker.io containerd runc -y
 
 # install docker from official repository
 apt update
-apt install ca-certificates curl gnupg lsb-release
+apt install ca-certificates curl gnupg lsb-release -y
 curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt update
-apt install docker-ce docker-ce-cli containerd.io
+apt install docker-ce docker-ce-cli containerd.io -y
 
 # install docker-compose
 curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
